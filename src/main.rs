be33c66e7d32875fd021321618cf16be23e85a12
ALinuxPerson <alinuxperson@gmail.com>
2022-01-20 12:36:27 +0800

    Implement log probabilities

--- src/main.rs
+++ src/main.rs
@@ -340,9 +340,24 @@ mod app {
         }
     }
 
+    use anyhow::Context;
     use crate::{NonEmptyStringFromStrAdapter, SynthTextTextCompletionMethod, TopKFromStrAdapter, TopPFromStrAdapter};
+    use owo_colors::OwoColorize;
 
-    pub async fn log_probabilities(context: String, continuation: NonEmptyStringFromStrAdapter) -> anyhow::Result<()> {
+    pub async fn log_probabilities(context: String, NonEmptyStringFromStrAdapter(continuation): NonEmptyStringFromStrAdapter) -> anyhow::Result<()> {
+        alp::info!("the provided context was: '{context}'");
+        alp::info!("the predicted continuation was: '{}'", continuation.inner());
+
+        let log_probabilities = crate::textsynth::engine()
+            .log_probabilities(context, continuation)
+            .await
+            .context("failed to connect to the textsynth api")?
+            .context("failed to get log probabilities")?;
+
+        alp::info!("log probability: {}", log_probabilities.log_probability().bold());
+        alp::info!("is greedy: {}", log_probabilities.is_greedy().bold());
+        alp::info!("total tokens: {}", log_probabilities.total_tokens().bold());
+
         Ok(())
     }
 
